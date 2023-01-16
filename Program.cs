785d4cd5b7d2849b4f816c96b4b38e423401e326
Iron Skull <ironskull89@mail.ru>
2023-01-17 01:48:49 +0300

    Added solution first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int SetNumber()
+{
+    Console.Write("Введите число: ");
+    if (!int.TryParse(Console.ReadLine(), out int number))
+    {
+        number = SetNumber();
+    }
+    return number;
+}
+
+int SetNaturalDegree()
+{
+    Console.Write("Введите натуральную степень числа: ");
+    if (!int.TryParse(Console.ReadLine(), out int number) || (number < 1))
+    {
+        number = SetNumber();
+    }
+    return number;
+}
+
+
+int number = SetNumber();
+int degree = SetNaturalDegree();
+
+int Exponentiation (int number, int degree)
+{
+    int exp = number; 
+    for (int i = 1; i < degree; i++) exp *= number;
+    return exp;
+}
+
+Console.WriteLine($"{number} в степени {degree} равна {Exponentiation(number, degree)}");
